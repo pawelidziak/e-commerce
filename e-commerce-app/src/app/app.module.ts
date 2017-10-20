e592d922a7e100f9bd5671325a5bcf188c73148c
Paweł Idziak <pa.idziak@gmail.com>
2017-10-20 14:39:47 +0200

    Dodanie bilbioteki angularfire2 oraz firebase

--- e-commerce-app/src/app/app.module.ts
+++ e-commerce-app/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import {HttpModule} from '@angular/http';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCxYvJHE-K5jZd5A5l2fy5olS9HA1tMq-I',
+  authDomain: 'e-commerce-pawel-idziak.firebaseapp.com',
+  databaseURL: 'https://e-commerce-pawel-idziak.firebaseio.com',
+  projectId: 'e-commerce-pawel-idziak',
+  storageBucket: 'e-commerce-pawel-idziak.appspot.com',
+  messagingSenderId: '100178337787'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
